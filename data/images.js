08e9dd48473d6ac6aff3bff87fcab2ebac71d19d
Darrick <dmckirnan@hotmail.com>
2017-09-06 14:34:52 -0700

    readded sample data files

---
+++ data/images.js
@@ -0,0 +1,15 @@
+const Images = {
+  '0': 'https://www.espressosupply.com/sites/all/files/imagecache/product_full/18520BL.png',
+  '1': 'https://images-na.ssl-images-amazon.com/images/I/31aiBakx4kL.jpg',
+  '2': 'https://img.domino.com/serve/58fb194105541b203598f9ee-w1000_h1000.jpg',
+  '3': 'http://www.homedepot.com/catalog/productImages/1000/f6/f662ef51-226e-449e-a2ab-c5cf83e06608_1000.jpg',
+  '4': 'http://media.4rgos.it/i/Argos/2328986_R_Z001A_UC17563188?$Web$&w=570&h=513&$WebPDPBadge570$&topright=empty&bottomleft=empty',
+  '5': 'https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5577/5577855_rd.jpg;maxHeight=550;maxWidth=642',
+  '6': 'https://images-na.ssl-images-amazon.com/images/I/51D8NXwQfvL._SY355_.jpg',
+  '7': 'https://target.scene7.com/is/image/Target/52320884?wid=520&hei=520&fmt=pjpeg',
+  '8': 'https://static.vibe.com/files/article_images/seattle-seahawks-super-bowl-xlviii-on-field-side-patch-59fifty-fitted-cap-new-era-nfl_2.jpg',
+  '9': 'https://images-na.ssl-images-amazon.com/images/I/61j2YmYHx9L._SY355_.jpg',
+  '10': 'https://images-na.ssl-images-amazon.com/images/I/917xadgCebL._SY445_.jpg',
+}
+
+module.exports = Images;
