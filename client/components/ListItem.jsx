d23e8c48f835c6757fb5382964327ce71829ed7f
Darrick <dmckirnan@hotmail.com>
2017-09-19 13:45:08 -0700

    corrected styles and flex layout of productList

--- client/components/ListItem.jsx
+++ client/components/ListItem.jsx
@@ -7,20 +7,34 @@ const ListItem = (props) => {
     let price = Number(applyDiscount(props.price));
     return (
     <li className="listItem">
-      <p>{props.name}</p>
-      <img src={props.image} />
-      <p className="presalePrice">{'$' + props.price}</p>
-      <p className="salePrice">{'$' + price}</p>
-      <button name={props.name} value={price} className="itemButton" onClick={props.addToCart}>Add to Order</button>
+      <div className="item-left">
+        <img src={props.image} />
+      </div>
+      <div className="item-center">
+        <h2>{props.name}</h2>
+        <p>This is test description, purposefully long so you have to style it you lazy ass.</p>
+      </div>
+      <div className="item-right">
+        <p className="presalePrice">{'$' + props.price}</p>
+        <p className="salePrice">{'$' + price}</p>
+        <button name={props.name} value={price} className="itemButton" onClick={props.addToCart}>Add to Order</button>
+      </div>
     </li>
     );
   }
   return (
     <li className="listItem">
-      <p onClick={props.toggleModal}>{props.name}</p>
-      <img src={props.image} />
-      <p>{props.price ? '$' + props.price : ''}</p>
-      <button name={props.name} value={props.price} className="itemButton" onClick={props.addToCart}>Add to Order</button>
+      <div className="item-left">
+        <img src={props.image} />
+      </div>
+      <div className="item-center">
+        <h2>{props.name}</h2>
+        <p>This is test description, purposefully long so you have to style it you lazy ass.</p>
+      </div>
+      <div className="item-right">
+        <p>{props.price ? '$' + props.price : ''}</p>
+        <button name={props.name} value={props.price} className="itemButton" onClick={props.addToCart}>Add to Order</button>
+      </div>
     </li>
   );
 };
